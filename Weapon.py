ce927a49b832c6ce501e7c01da536664fcb6d52a
sava <savadyagilev@yandex.ru>
2020-12-23 18:40:30 +0300

    minor changes

--- Weapon.py
+++ Weapon.py
@@ -1,13 +1,16 @@
 class Weapon:
-    def __init__(self, name, damage, ignoring_armor=False):
+    def __init__(self, name, damage, ignoring_armor=0): # ignoring_armor is int
         self.name = name
         self.damage = damage
-        self.ignoring_armor = ignoring_armor
+        self.ignoring_armor = ignoring_armor # сколько очков брони он игнорирует
         # название : урон
         self.weapon_lib = {
             "Меч": 5,
             "Булава": 4,
             "Посох": 3}
+
+    def get_name(self):
+        return self.name
 
     def get_damage(self):
         return self.damage
