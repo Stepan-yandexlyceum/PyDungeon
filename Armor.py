f0f9ecb9ae7c664a8617abe1e5b6b25d69094b46
sava <savadyagilev@yandex.ru>
2020-12-23 18:40:16 +0300

    minor changes

--- Armor.py
+++ Armor.py
@@ -1,10 +1,14 @@
 class Armor:
-    def __init__(self, health_bonus, protection):
+    def __init__(self, name, health_bonus, protection):
         self.health_bonus = health_bonus
         self.protection = protection
+        self.name = name
 
     def get_health_bonus(self):
         return self.health_bonus
 
     def get_protection(self):
         return self.protection
+
+    def get_name(self):
+        return self.name
