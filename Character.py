6a65227db2b34caabba7d60821693003e5f49dae
sava <savadyagilev@yandex.ru>
2020-12-23 18:42:10 +0300

    now inherited from Sprite

--- Character.py
+++ Character.py
@@ -1,8 +1,9 @@
 import pygame
 
 
-class Character:
-    def __init__(self, weapon, armor):
+class Character(pygame.sprite.Sprite):
+    def __init__(self, weapon, armor, sprites_grop):
+        super().__init__(sprites_grop)
         self.weapon = weapon
         self.armor = armor
         self.x = 0 # TODO: генерировать это в соответствии с условиями
