9bae337f6edd5c75bd8d31d7415957a475247b11
sava <savadyagilev@yandex.ru>
2020-12-24 18:32:32 +0300

    now the knight "looks" where he is moving

--- player.py
+++ player.py
@@ -4,8 +4,7 @@ import pygame
 
 
 class Player(Character):
-    image = pygame.image.load("data/Knight_01__IDLE_000.png")
-    image = pygame.transform.scale(image, (150, 150))
+    image = pygame.image.load("data/Knight_01_right.png")
 
     def __init__(self, weapon, armor, screen, sprites):
         super().__init__(weapon, armor, sprites)
@@ -21,16 +20,21 @@ class Player(Character):
             self.rect.y += 10
         if keys[pygame.K_a]:
             self.rect.x -= 10
+            self.image = pygame.image.load("data/Knight_01_left.png")
         if keys[pygame.K_s]:
             self.rect.y -= 10
         if keys[pygame.K_d]:
             self.rect.x += 10
+            self.image = pygame.image.load("data/Knight_01_right.png")
 
         if keys[pygame.K_DOWN]:
             self.rect.y += 10
         if keys[pygame.K_LEFT]:
             self.rect.x -= 10
+            self.image = pygame.image.load("data/Knight_01_left.png")
         if keys[pygame.K_UP]:
             self.rect.y -= 10
         if keys[pygame.K_RIGHT]:
             self.rect.x += 10
+            self.image = pygame.image.load("data/Knight_01_right.png")
+
