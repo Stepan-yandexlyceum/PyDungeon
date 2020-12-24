9f509bae14eea2ea00a367d67228e08f4eba5592
sava <savadyagilev@yandex.ru>
2020-12-24 21:20:12 +0300

    now the player does not walk through walls

--- player.py
+++ player.py
@@ -1,3 +1,4 @@
+from map import text_map
 from settings import *
 from Character import *
 import pygame
@@ -10,34 +11,56 @@ class Player(Character):
     def __init__(self, weapon, armor, screen, sprites):
         super().__init__(weapon, armor, sprites)
         self.x, self.y = player_pos
-        self.rect = self.image.get_rect()
+        self.rect = pygame.Rect(self.x * 32, self.y * 32, 32, 32)
         self.direction = 'right'
+
+    def pos(self):
+        return self.x, self.y
 
     def movement(self):
         keys = pygame.key.get_pressed()
 
+        y = self.rect.y
+        x = self.rect.x
+        m_y = self.y
+        m_x = self.x
         # управление игроком производится клавишами w,a,s,d
         if keys[pygame.K_w]:
             self.rect.y += -32
+            self.y -= 1
         if keys[pygame.K_a]:
             self.rect.x -= 32
+            self.x -= 1
             self.flip_image('left')
         if keys[pygame.K_s]:
-            self.rect.y -= -32
+            self.rect.y += 32
+            self.y += 1
         if keys[pygame.K_d]:
             self.rect.x += 32
+            self.x += 1
             self.flip_image('right')
 
         if keys[pygame.K_DOWN]:
             self.rect.y += 32
+            self.y += 1
         if keys[pygame.K_LEFT]:
             self.rect.x -= 32
+            self.x -= 1
             self.flip_image('left')
         if keys[pygame.K_UP]:
             self.rect.y -= 32
+            self.y -= 1
         if keys[pygame.K_RIGHT]:
             self.rect.x += 32
+            self.x += 1
             self.flip_image('right')
+
+        if text_map[self.x][self.y] == 'w':
+            self.rect.y = y
+            self.rect.x = x
+            self.y = m_y
+            self.x = m_x
+            
 
     def flip_image(self, direction):
         if direction != self.direction:
